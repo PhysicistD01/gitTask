acf6fa3367bcceeb234d0c2c90dd0af935786906
Physicist <22284168@sun.ac.za>
2023-07-21 22:06:04 +0200

    Changed the javascript program to display the user input on the webpage. Added the html and css files to the program files

--- task1_project/helloWorld.js
+++ task1_project/helloWorld.js
@@ -1,5 +1,10 @@
-/*This is a javascript program that accepts user input and prints to the console*/
+/*This is a javascript program that accepts user input and prints to the web page*/
 
-let myInput = prompt('Enter your message:')
+//Define the function that is activated when the submit button clicked.
+function myFunction(){
+    let userInput = document.querySelector("#userInput");
+    let message = document.querySelector("#message");
 
-console.log(myInput);
+    message.innerHTML = userInput.value;        //Print the output to the webpage
+    userInput.value = '';                       //Clear the input field after submission.
+}
